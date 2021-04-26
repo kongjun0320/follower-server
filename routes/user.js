05360902dde587b1f1556ecd8616b972cdffedb8
kongjun0320 <1741142595@qq.com>
2021-04-26 22:58:22 +0800

    用户CRUD权限注释

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router')
 const jwt = require('koa-jwt')
 
-const { find, login, create, findById, checkOwner, update } = require('../controllers/user')
+const { find, login, create, findById, checkOwner, delete: del,update } = require('../controllers/user')
 
 const { secret } = require('../config')
 
@@ -14,7 +14,9 @@ const auth = jwt({ secret })
 router.get('/', find)
 router.get('/:id', findById)
 router.post('/', create)
-router.patch('/:id', auth, checkOwner, update)
+router.delete('/:id', del)
+router.patch('/:id', update)
+// router.patch('/:id', auth, checkOwner, update)
 router.post('/login', login)
 
 module.exports = router
